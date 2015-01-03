b6ba75bbbd177ada68d3f85fc7db188b01bd71f7
michel_petrovic <michel_petrovic@192.168.4.15>
2015-01-03 08:28:51 +0100

    Added a header with comments for all groovy and java classes

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,3 +1,21 @@
+/*  <MiaMiam: Web application for publishing recipes
+    Copyright (C) <2014>  <Michel Petrovic> <email: dotnetmobile@gmail.com>
+
+    Contributor(s): _____________________.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 // locations to search for config files that get merged into the main config;
 // config files can be ConfigSlurper scripts, Java properties files, or classes
 // in the classpath in ConfigSlurper format
